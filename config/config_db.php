11174495205e4ce0100aef2f66f65c1dd973bfac
yosral <yosral@yosrals-MacBook-Pro.local>
2022-12-04 15:43:20 +0700

    minor debugging: config_db.php empty

---
+++ config/config_db.php
@@ -0,0 +1,12 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = '10.0.26.6';
+   public $dbuser = 'halodev';
+   public $dbpassword = 'bh!nneKa1';
+   public $dbdefault = 'glpi_dev';
+   public $use_timezones = true;
+   public $use_utf8mb4 = true;
+   public $allow_myisam = false;
+   public $allow_datetime = false;
+   public $allow_signed_keys = false;
+}
