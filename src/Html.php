6c50060b3384ca3ddefb527dc1765ce55e3abff5
yosral <yosral@yosrals-MacBook-Pro.local>
2023-01-17 13:52:13 +0700

    19 add about page; continue making pop-up alerts

--- src/Html.php
+++ src/Html.php
@@ -1555,7 +1555,7 @@ HTML;
 
         $menu = [
             'about' => [
-                'default' => '/front/aboutus.php',
+                'default' => '/front/about_us.php',
                 'title'   => __('About'),
                 'icon'    => 'fas fa-info-circle',
             ],
