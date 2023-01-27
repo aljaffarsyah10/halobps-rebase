d334b892293f9d723e589506cf4ce0c2da734bc9
Rihan Yosral - 340058946 <yosral@bps.go.id>
2023-01-27 22:30:01 +0700

    Undo database failed. Self-asignee failure T_T

--- config/config_db.php
+++ config/config_db.php
@@ -1,8 +1,8 @@
 <?php
 class DB extends DBmysql {
-   public $dbhost = 'localhost';
-   public $dbuser = 'root';
-   public $dbpassword = '';
+   public $dbhost = '10.0.26.6';
+   public $dbuser = 'halodev';
+   public $dbpassword = 'bh!nneKa1';
    public $dbdefault = 'glpi_dev';
    public $use_timezones = true;
    public $use_utf8mb4 = true;
