03f7eac2e0183238a79779a274a5703c5ea3e942
yosral <yosral@bps.go.id>
2023-02-17 22:58:07 +0700

    related to #49, #50, #51, #53, #54

--- config/config_db.php
+++ config/config_db.php
@@ -1,9 +1,9 @@
 <?php
 class DB extends DBmysql {
-   public $dbhost = '10.0.26.6';
-   public $dbuser = 'halodev';
-   public $dbpassword = 'bh!nneKa1';
-   public $dbdefault = 'glpi_dev';
+   public $dbhost = 'localhost';
+   public $dbuser = 'root';
+   public $dbpassword = '';
+   public $dbdefault = 'halosis';
    public $use_timezones = true;
    public $use_utf8mb4 = true;
    public $allow_myisam = false;
